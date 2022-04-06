7c3e07a971014663826866b9258e2461c021bc4a
J-JE <leehj9709@gmail.com>
2022-04-06 15:39:52 +0900

    JJE/ driver.properties, Attachment.java 추가

--- src/main/java/com/uni/common/Attachment.java
+++ src/main/java/com/uni/common/Attachment.java
@@ -1,5 +1,101 @@
 package com.uni.common;
 
+import java.sql.Date;
+
 public class Attachment { //참조파일 dto -> recipe, store, survey에서 사용됨
+	private int fileNo;			// 파일 번호
+	private int category;		// 게시판 분류 번호
+	private int refBoardNo;		// 참조 게시글 번호
+	private String originName;	// 파일 원본명
+	private String changeName;	// 파일 수정명
+	private String filePath;	// 저장폴더경로
+	private Date uploadDate;	// 업로드일
+	private String status;		// 파일 상태값
+
+	public Attachment() {	}
+	
+	public Attachment(int fileNo, int category, int refBoardNo, String originName, String changeName, String filePath,
+			Date uploadDate, String status) {
+		super();
+		this.fileNo = fileNo;
+		this.category = category;
+		this.refBoardNo = refBoardNo;
+		this.originName = originName;
+		this.changeName = changeName;
+		this.filePath = filePath;
+		this.uploadDate = uploadDate;
+		this.status = status;
+	}
+
+	public int getFileNo() {
+		return fileNo;
+	}
+
+	public void setFileNo(int fileNo) {
+		this.fileNo = fileNo;
+	}
+
+	public int getCategory() {
+		return category;
+	}
+
+	public void setCategory(int category) {
+		this.category = category;
+	}
+
+	public int getRefBoardNo() {
+		return refBoardNo;
+	}
+
+	public void setRefBoardNo(int refBoardNo) {
+		this.refBoardNo = refBoardNo;
+	}
+
+	public String getOriginName() {
+		return originName;
+	}
+
+	public void setOriginName(String originName) {
+		this.originName = originName;
+	}
+
+	public String getChangeName() {
+		return changeName;
+	}
+
+	public void setChangeName(String changeName) {
+		this.changeName = changeName;
+	}
+
+	public String getFilePath() {
+		return filePath;
+	}
+
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+
+	public Date getUploadDate() {
+		return uploadDate;
+	}
+
+	public void setUploadDate(Date uploadDate) {
+		this.uploadDate = uploadDate;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Attachment [fileNo=" + fileNo + ", category=" + category + ", refBoardNo=" + refBoardNo
+				+ ", originName=" + originName + ", changeName=" + changeName + ", filePath=" + filePath
+				+ ", uploadDate=" + uploadDate + ", status=" + status + "]";
+	}
 	
 }
--- src/main/java/com/uni/recipe/model/dto/Recipe.java
+++ src/main/java/com/uni/recipe/model/dto/Recipe.java
@@ -2,4 +2,109 @@ package com.uni.recipe.model.dto;
 
 public class Recipe {
 
+	private int recipeNo;	//레시피번호
+	private int category;	//게시판분류번호
+	private String recipeTitle;	//레시피제목
+	private String recipeContent;	//레시피내용
+	private String recipeTag;	//레시피 태그
+	private String recipePro;	//레시피 재료
+	private int rCategoryNo;	//카테고리번호
+	private int count;	//조회수
+	private String status;	//상태값
+	
+	public Recipe() {	}
+
+	public Recipe(int recipeNo, int category, String recipeTitle, String recipeContent, String recipeTag,
+			String recipePro, int rCategoryNo, int count, String status) {
+		super();
+		this.recipeNo = recipeNo;
+		this.category = category;
+		this.recipeTitle = recipeTitle;
+		this.recipeContent = recipeContent;
+		this.recipeTag = recipeTag;
+		this.recipePro = recipePro;
+		this.rCategoryNo = rCategoryNo;
+		this.count = count;
+		this.status = status;
+	}
+
+	public int getRecipeNo() {
+		return recipeNo;
+	}
+
+	public void setRecipeNo(int recipeNo) {
+		this.recipeNo = recipeNo;
+	}
+
+	public int getCategory() {
+		return category;
+	}
+
+	public void setCategory(int category) {
+		this.category = category;
+	}
+
+	public String getRecipeTitle() {
+		return recipeTitle;
+	}
+
+	public void setRecipeTitle(String recipeTitle) {
+		this.recipeTitle = recipeTitle;
+	}
+
+	public String getRecipeContent() {
+		return recipeContent;
+	}
+
+	public void setRecipeContent(String recipeContent) {
+		this.recipeContent = recipeContent;
+	}
+
+	public String getRecipeTag() {
+		return recipeTag;
+	}
+
+	public void setRecipeTag(String recipeTag) {
+		this.recipeTag = recipeTag;
+	}
+
+	public String getRecipePro() {
+		return recipePro;
+	}
+
+	public void setRecipePro(String recipePro) {
+		this.recipePro = recipePro;
+	}
+
+	public int getrCategoryNo() {
+		return rCategoryNo;
+	}
+
+	public void setrCategoryNo(int rCategoryNo) {
+		this.rCategoryNo = rCategoryNo;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Recipe [recipeNo=" + recipeNo + ", category=" + category + ", recipeTitle=" + recipeTitle
+				+ ", recipeContent=" + recipeContent + ", recipeTag=" + recipeTag + ", recipePro=" + recipePro
+				+ ", rCategoryNo=" + rCategoryNo + ", count=" + count + ", status=" + status + "]";
+	}
+	
 }
