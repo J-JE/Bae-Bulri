a328b67245e36e7717b331be0bdc4f361b83759b
J-JE <leehj9709@gmail.com>
2022-04-08 21:23:57 +0900

    JJE /test

--- src/main/java/com/uni/likey/model/service/LikeyService_jje.java
+++ src/main/java/com/uni/likey/model/service/LikeyService_jje.java
@@ -5,5 +5,7 @@ import static com.uni.common.JDBCTemplate.*;
 import java.sql.Connection;
 
 public class LikeyService_jje {
-
+	public LikeyService_jje() {
+		// TODO Auto-generated constructor stub
+	}
 }
