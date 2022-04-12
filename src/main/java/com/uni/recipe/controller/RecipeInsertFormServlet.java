fca7d4b0d84087ce7554d29b70196714c30b1a62
baekjiwoo <nnowerp@gmail.com>
2022-04-12 21:28:49 +0900

    JW / 일부 사항 수정 /

--- src/main/java/com/uni/recipe/controller/RecipeInsertFormServlet.java
+++ src/main/java/com/uni/recipe/controller/RecipeInsertFormServlet.java
@@ -26,8 +26,8 @@ public class RecipeInsertFormServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("views/recipe/recipeInsertForm.jsp").forward(request, response);
 		// 화면 전환만 진행
+		request.getRequestDispatcher("views/recipe/recipeInsertForm.jsp").forward(request, response);
 	}
 
 	/**
